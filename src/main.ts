1a3db137873690e163b2696aee511f50b42dd440
Miad Abdi <50056491+miadabdi@users.noreply.github.com>
2022-09-09 17:47:31 +0430

    <feat>(security, versioning, cookies): enabled cors, added cookie-parser to use cookies with jwt, added helmet, added compression, added api versioning, so we can keep previous logics

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import * as compression from 'compression';
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { API_PREFIX, LOG_LEVELS } from './common/constants';
+import { AllExceptionsFilter } from './common/exceptions';
+import { LoggingInterceptor, TimeoutInterceptor } from './common/interceptors';
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: LOG_LEVELS,
+  });
+
+  app.use(helmet());
+  app.use(compression());
+  app.use(cookieParser());
+
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,9 +27,22 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix(API_PREFIX);
 
-  await app.listen(3000);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  app.useGlobalInterceptors(new LoggingInterceptor(), new TimeoutInterceptor());
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT');
+  await app.listen(port);
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
