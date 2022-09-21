5b11fc2dccdc9f011869a6461e604c2da4c8510e
Miad Abdi <50056491+miadabdi@users.noreply.github.com>
2022-09-21 23:20:11 +0330

    feat(mail): added a new method to send plain email
    feat(db): added a seed file to seed the database

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -7,13 +7,22 @@ import { anyObject } from '../common/types';
 export class MailService {
 	constructor(private readonly mailerService: MailerService) {}
 
-	send(to: string, subject: string, template: string, context: anyObject): Promise<SentMessageInfo> {
+	sendHtml(to: string, subject: string, template: string, context: anyObject): Promise<SentMessageInfo> {
 		return this.mailerService.sendMail({
 			to,
 			// from: 'noreply@nestjs.com',
 			subject,
 			template: __dirname + `/${template}`, // The `.pug`, `.ejs` or `.hbs` extension is appended automatically.
 			context,
+		});
+	}
+
+	sendPlain(to: string, subject: string, message: string): Promise<SentMessageInfo> {
+		return this.mailerService.sendMail({
+			to,
+			// from: 'noreply@nestjs.com',
+			subject,
+			text: message,
 		});
 	}
 }
