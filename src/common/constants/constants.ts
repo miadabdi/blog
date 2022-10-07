71c461b845d8f9be323a9d674588cbb665bb07ac
Miad Abdi <50056491+miadabdi@users.noreply.github.com>
2022-10-07 15:08:32 +0330

    feat(minio): adding bucket definitions in constants.ts

--- src/common/constants/constants.ts
+++ src/common/constants/constants.ts
@@ -9,3 +9,52 @@ export const LOG_LEVELS = {
 	verbose: 1,
 } as const;
 export const API_PREFIX = 'api';
+export const BUCKETS = [
+	{
+		name: 'images',
+		policy: {
+			Version: '2012-10-17',
+			Statement: [
+				{
+					Sid: 'PublicRead',
+					Effect: 'Allow',
+					Principal: '*',
+					Action: ['s3:GetObject', 's3:GetObjectVersion'],
+					Resource: [`arn:aws:s3:::images/*`],
+				},
+			],
+		},
+		allowedMimeTypes: ['image/apng', 'image/avif', 'image/gif', 'image/jpeg', 'image/png', 'image/webp'],
+	},
+	{
+		name: 'public',
+		policy: {
+			Version: '2012-10-17',
+			Statement: [
+				{
+					Sid: 'PublicRead',
+					Effect: 'Allow',
+					Principal: '*',
+					Action: ['s3:GetObject', 's3:GetObjectVersion'],
+					Resource: [`arn:aws:s3:::public/*`],
+				},
+			],
+		},
+		allowedMimeTypes: [
+			'image/apng',
+			'image/avif',
+			'image/gif',
+			'image/jpeg',
+			'image/png',
+			'image/webp',
+			'application/octet-stream',
+			'text/plain',
+			'text/css',
+			'text/html',
+			'text/javascript',
+			'application/javascript',
+		],
+	},
+] as const;
+export const BUCKET_NAMES = BUCKETS.map((bucket) => bucket.name);
+export type BUCKET_NAMES_TYPE = typeof BUCKET_NAMES[0];
