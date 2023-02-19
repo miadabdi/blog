06acda944621047b0209ed6df1f54005f6486a0e
Miad Abdi <50056491+miadabdi@users.noreply.github.com>
2023-02-20 00:48:16 +0330

    feat(prisma): added comments migration

---
+++ prisma/migrations/20230219190822_added_comment_schema/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "comments" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+    "content" TEXT NOT NULL,
+    "author_id" INTEGER NOT NULL,
+    "post_id" INTEGER NOT NULL,
+    "reply_to" INTEGER,
+
+    CONSTRAINT "comments_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_reply_to_fkey" FOREIGN KEY ("reply_to") REFERENCES "comments"("id") ON DELETE SET NULL ON UPDATE CASCADE;
