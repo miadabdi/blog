d0dc7edb2e44d6cc181901b0f5beb25b773e2efe
Miad Abdi <miadabdi80@gmail.com>
2025-06-28 20:50:02 +0330

    feat(post): check for slug's uniqeness

--- src/post/service.py
+++ src/post/service.py
@@ -2,6 +2,7 @@ from functools import lru_cache
 from typing import Annotated
 
 from fastapi import Depends, HTTPException, status
+from sqlalchemy.exc import IntegrityError
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from .models import Post
@@ -14,8 +15,21 @@ class PostService:
         self.repository = repo
 
     async def create_post(self, data: CreatePost, session: AsyncSession) -> Post:
-        post_record = await self.repository.create(data.model_dump(), session)
-        await session.commit()
+        try:
+            post_record = await self.repository.create(data.model_dump(), session)
+        except IntegrityError as e:
+            print(f"IntegrityError: {e.orig}")
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Post with this title already exists.",
+            )
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="An unexpected error occurred while creating the post.",
+            )
+
         return post_record
 
     async def update_post(
@@ -23,17 +37,14 @@ class PostService:
     ) -> Post:
         post_data = update_data.model_dump(exclude_unset=True)
         updated_post = await self.repository.update(id, post_data, session)
-        await session.commit()
         return updated_post
 
     async def delete_post(self, id: int, session: AsyncSession) -> Post:
         result = await self.repository.delete(id, session)
-        await session.commit()
         return result
 
     async def get_post_by_id(self, id: int, session: AsyncSession) -> Post:
         result = await self.repository.get_by_id(id, session)
-        await session.commit()
 
         if result is None:
             raise HTTPException(
